ecfd82dd760b6e63030f7209913202d388236823
huangmeixiqi <1600582963@qq.com>
2019-03-22 21:23:00 +0800

    四则运算2.0

---
+++ 四则/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x; int y;
+            Console.Write("请输入第一个数");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("请输入第一个数");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            hy(x, y);
+            sub(x, y);
+            nul(x, y);
+            div(x, y);
+
+            Console.ReadLine();
+        }
+        static void hy(int x, int y)
+        {
+            Console.WriteLine("{0}+{1}={2}", x, y, x + y);
+        }
+        static void sub(int x, int y)
+        {
+            Console.WriteLine("{0}-{1}={2}", x, y, x - y);
+        }
+        static void nul(int x, int y)
+        {
+            Console.WriteLine("{0}*{1}={2}", x, y, x * y);
+        }
+        static void div(int x, int y)
+        {
+            try
+            {
+                Console.WriteLine("{0}/{1}={2}", x, y, x / y);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("输入有误" + ex.Message);//22521556454656
+            }
+        }
+
+    }
+}
